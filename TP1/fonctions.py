0f181113b3e1c6ec7cb58d8811fab57ece2b8037
Morgan2304 <morgan.sailly@sfr.fr>
2025-09-03 15:10:24 +0200

    puissance par 0

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -3,3 +3,5 @@ def puissance (a,b):
 		raise TypeError("Seulement les entier sont autorise")
 	return a**b
 
+	if a==0 and b<0 :
+		raise ValueError ("Impossible de divisé faire la puissance par 0")
