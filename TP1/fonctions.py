73415ecd98a7efb62168f856a2072167dce9dc88
Morgan2304 <morgan.sailly@sfr.fr>
2025-09-03 15:59:30 +0200

    fin de séance

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -3,5 +3,13 @@ def puissance (a,b):
 		raise TypeError("Seulement les entier sont autorise")
 	if a==0 and b<0:
 		raise ValueError("pas de puissance negative pour 0")
-			
+
+	for i in range (b):
+		resultat *= a
+	if b > 0:
+		return resultat
+	if b < 0:
+		return 1/resultat
+
+
 	return a**b
