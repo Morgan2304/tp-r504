ed24929e876d2ac83f294f462da00a2e81c7f168
Morgan2304 <morgan.sailly@sfr.fr>
2025-09-11 15:10:50 +0200

    V1.0

--- TP4/build_image.sh
+++ TP4/build_image.sh
@@ -1,2 +1,2 @@
-#2.9 /Script permettant de lancer la construction de l'image 
+#2.9 /Script permettant de lancer la construction de l'image depuis le dockerfile
 docker build -f Dockerfile1 -t im-tp4 .
---
+++ TP4/run_app.sh
@@ -0,0 +1,6 @@
+#2.10 /Lancement du conteneur
+docker run -d \
+  --name tp4-app \
+  --network net-tp4 \
+  -p 5000:5000 \
+  im-tp4
