1285d0176361797499225b33754c569c344c4218
Morgan2304 <morgan.sailly@sfr.fr>
2025-10-15 14:36:41 +0200

    maj

---
+++ TP9/ClientHTTP.java
@@ -0,0 +1,36 @@
+import java . io . * ;
+import java . net . * ;
+
+public class ClientHTTP
+{
+	public static void main( String args[] ) throws Exception
+	{
+		try
+		{
+			Socket socket = new Socket( args[0], 80);
+
+			OutputStreamWriter osw = new OutputStreamWriter(socket.getOutputStream());
+			InputStreamReader isw = new InputStreamReader(socket.getInputStream());
+
+			BufferedWriter bufOut = new BufferedWriter(osw);
+			BufferedReader bufIn = new BufferedReader(isw);
+
+			String request = "GET / HTTP/1.1\r\n\r\n"; // requete HTTP
+			bufOut.write(request, 0, request.length());
+			bufOut.flush();
+
+			String line = bufIn.readLine(); // lecture ligne par ligne
+			while(line != null) { // tant qu'il y a des données recues,
+			    System.out.println(line); // ... les afficher
+			    line = bufIn.readLine();
+			}
+			bufIn.close();
+			bufOut.close();
+			socket.close();
+			}
+		catch (Exception e)
+		{
+			System.out.println("Erreur");
+		}
+	}
+}
